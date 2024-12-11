fa8c940558601e80e4cfb02e69b238c12fcd28ff
Chethan L <chethan.shantha@gmail.com>
2024-12-11 19:36:04 +0530

    adding lua51 for neovim

--- neovim/default.nix
+++ neovim/default.nix
@@ -1,22 +1,23 @@
 {config, pkgs, inputs,... }: {
 
-	nixpkgs.overlays = [ inputs.neovim-nightly-overlay.overlay.default ];
-	home.packages = with pkgs; [
-		neovim-remote
-			nixfmt-rfc-style
-			luajitPackages.luarocks-nix
-			lua-language-server
-			luajitPackages.jsregexp
-            luajitPackages.magick
-			stylua
-			inputs.neovim-nightly-overlay.packages.${pkgs.system}.default
-	];
+    nixpkgs.overlays = [ inputs.neovim-nightly-overlay.overlay.default ];
+    home.packages = with pkgs; [
+        neovim-remote
+        nixfmt-rfc-style
+        lua51Packages.lua
+        luajitPackages.luarocks-nix
+        lua-language-server
+        luajitPackages.jsregexp
+        luajitPackages.magick
+        stylua
+        inputs.neovim-nightly-overlay.packages.${pkgs.system}.default
+    ];
 
     xdg.configFile.neovim = {
         enable = true;
         recursive = true;
-		source = config.lib.file.mkOutOfStoreSymlink ../neovim;
-		target = "nvim";
-	};
+        source = config.lib.file.mkOutOfStoreSymlink ../neovim;
+        target = "nvim";
+    };
 
 }
