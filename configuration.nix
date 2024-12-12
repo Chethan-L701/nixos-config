d227b1756c63a985d204a27f234fc52b740f90a2
Chethan L <chethan.shantha@gmail.com>
2024-12-12 21:07:08 +0530

    setting ranger env var in configuration.nix file

--- configuration.nix
+++ configuration.nix
@@ -272,6 +272,7 @@
 		MOZ_ENABLE_WAYLAND = "1";
 		XCURSOR_THEME = "catppuccin-mocha-dark-cursors";
 		STEAM_EXTRA_COMPAT_TOOLS_PATHS = "\${HOME}/.steam/root/compatibilitytools.d";
+        RANGER_LOAD_DEFUALT_RC = false;
 	};
 
 # Some programs need SUID wrappers, can be configured further or are
